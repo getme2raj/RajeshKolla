e1041dde85535efcac2f4da922630efff6246a14
Rajesh Kolla <rajesh.kolla@octanner.com>
2013-06-25 07:48:20 -0600

    init

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: rajesh.kolla
+ * Date: 6/25/13
+ * Time: 7:46 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+    public static void main (String args[]) {
+        System.out.println("Hello World!!");
+    }
+}
